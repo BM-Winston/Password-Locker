9b5d2a0d5b6cdbcccbb1c60538f920bdd0766e7d
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-25 12:59:17 +0300

    Add test for credential class

--- passwordl_test.py
+++ passwordl_test.py
@@ -2,4 +2,55 @@ import unittest
 from passwordl import User, Credentials
 
 
-class TestUser(unittest.Testcase)
+class TestUser(unittest.TestCase):
+    """
+    create a subclass that defines test cases
+    """
+
+
+    def setup(self):
+        """
+        first test
+        """
+        self new_user = User("Winston, abcdefgh")
+
+
+    def test_init():
+        """
+        test to check if object is initialized correctly
+        """
+
+        self.asserEqual(self.new_user.username, "Winston")
+        self.asserEqual(sel.new_user.password,"abcdefgh")
+
+
+    def test_save_user(self):
+        """
+        test_save_user test case to test if the user object is saved into
+         the user_list
+        """
+        self.new_user.save_user
+        self.asserEqual(len(User.user_list)1)
+
+
+    def tearDown(self):
+        """
+        method that cleans after each test is run
+        """
+        User.user_list = []
+
+
+class TestCredentials(unittest.TestCase)
+
+
+    
+
+
+    
+
+    
+
+
+    
+
+
